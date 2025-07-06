686d82a66789c880e40ceb20065cc42d8d372e64
sryang <sarang628@gmail.com>
2025-07-06 12:18:18 +0800

    update

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -6,42 +6,54 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sarang.library.compose.DetailRestaurantInfo
+import com.sarang.library.compose.LocalDetailRestaurantInfo
+import com.sarang.library.compose.LocalRestaurantDetailImageLoader
+import com.sarang.library.compose.RestaurantDetailImageLoader
+import com.sarang.library.compose.restaurantdetail.RestaurantDetailScreen
+import com.sarang.torang.di.image.provideTorangAsyncImage
+import com.sarang.torang.di.restaurant_info.RestaurantInfoWithPermission
 import com.sarang.torang.ui.theme.RestaurantOverViewTheme
+import com.sryang.library.compose.workflow.BestPracticeViewModel
+import com.sryang.torang.ui.TorangTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            RestaurantOverViewTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            TorangTheme {
+                CompositionLocalProvider(
+                    LocalRestaurantDetailImageLoader provides restaurantDetailImageLoader,
+                    LocalDetailRestaurantInfo provides customRestaurantInfo,
+                    LocalRestaurantInfoImageLoader provides restaurantImageLoader
+                ) {
+                    RestaurantDetailScreen(restaurantId = 234)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+val restaurantDetailImageLoader: RestaurantDetailImageLoader = { modifier, url, width, height, scale ->
+    // 여기서 실제 이미지 로딩 구현 예시
+    provideTorangAsyncImage().invoke(modifier, url, width, height, scale)
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RestaurantOverViewTheme {
-        Greeting("Android")
-    }
+val customRestaurantInfo: DetailRestaurantInfo = {
+    RestaurantInfoWithPermission(restaurantId = it, viewModel = BestPracticeViewModel())
+}
+
+val restaurantImageLoader: RestaurantInfoImageLoader = { modifier, url, width, height, scale ->
+    // 여기서 실제 이미지 로딩 구현 예시
+    provideTorangAsyncImage().invoke(modifier, url, width, height, scale)
 }
--- app/src/main/java/com/sarang/torang/RestaurantOverViewApplication.kt
+++ app/src/main/java/com/sarang/torang/RestaurantOverViewApplication.kt
@@ -1,3 +1,5 @@
+package com.sarang.torang
+
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
