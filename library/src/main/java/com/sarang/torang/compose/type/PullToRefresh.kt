f1d67df322b6444a3edf4254a5fa5e82bf536c6b
sryang <sarang628@gmail.com>
2025-09-07 16:05:17 +0800

    add pull to refresh

--- library/src/main/java/com/sarang/torang/compose/type/PullToRefresh.kt
+++ library/src/main/java/com/sarang/torang/compose/type/PullToRefresh.kt
@@ -2,9 +2,11 @@ package com.sarang.torang.compose.type
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Modifier
 
 
 typealias PullToRefresh = @Composable (
+    modifier : Modifier,
     isRefreshing: Boolean,
     onRefresh: (() -> Unit),
     contents: @Composable () -> Unit
@@ -12,7 +14,7 @@ typealias PullToRefresh = @Composable (
 
 val LocalPullToRefresh = compositionLocalOf<PullToRefresh> {
     // 기본 구현: 경고 로그 출력
-    @Composable { isRefreshing, onRefresh, contents ->
+    @Composable { modifier, isRefreshing, onRefresh, contents ->
         contents.invoke()
     }
 }
