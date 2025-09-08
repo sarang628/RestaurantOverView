4208692bbb930be332ace872e28c399947b93cb8
sryang <sarang628@gmail.com>
2025-09-08 18:24:15 +0800

    refactor : remove unused import

--- app/src/main/java/com/sarang/torang/RestaurantOverViewApplication.kt
+++ app/src/main/java/com/sarang/torang/RestaurantOverViewApplication.kt
@@ -1,12 +1,6 @@
 package com.sarang.torang
 
 import android.app.Application
-import androidx.compose.runtime.CompositionLocalProvider
-import com.sarang.torang.compose.type.RestaurantOverViewImageLoader
-import com.sarang.torang.compose.type.RestaurantOverviewRestaurantInfo
-import com.sarang.torang.di.image.provideTorangAsyncImage
-import com.sarang.torang.di.restaurant_info.RestaurantInfoWithPermission
-import com.sryang.library.compose.workflow.BestPracticeViewModel
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
