31c3a8da9110ddc5afcd3ea53631ac5b590ed5f7
sryang <sarang628@gmail.com>
2025-11-24 15:49:22 +0900

    add like feature

--- library/src/main/java/com/sarang/torang/compose/restaurantdetail/feed/RestaurantFeedsViewModel.kt
+++ library/src/main/java/com/sarang/torang/compose/restaurantdetail/feed/RestaurantFeedsViewModel.kt
@@ -9,26 +9,42 @@ import androidx.lifecycle.viewModelScope
 import com.sarang.torang.data.FeedInRestaurant
 import com.sarang.torang.usecase.restaurantoverview.FetchReviewsUseCase
 import com.sarang.torang.usecase.restaurantoverview.ClickLikeUseCase
+import com.sarang.torang.usecase.restaurantoverview.IsLoginUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RestaurantFeedsViewModel @Inject constructor(
     val fetchReviewUseCase  : FetchReviewsUseCase,
-    val clickLikeUseCase       : ClickLikeUseCase
+    val clickLikeUseCase    : ClickLikeUseCase,
+    val isLoginUseCase      : IsLoginUseCase
 ) : ViewModel() {
 
     var uiState: List<FeedInRestaurant> by mutableStateOf(ArrayList())
+    val isLogin : StateFlow<Boolean> = isLoginUseCase.invoke().stateIn(
+        scope = viewModelScope, initialValue = false, started = SharingStarted.WhileSubscribed(5000)
+    )
     val tag = "__RestaurantFeedsViewModel"
     private var _errorMessage : MutableStateFlow<List<String>> = MutableStateFlow(listOf());
     var errorMessage : StateFlow<List<String>> = _errorMessage
+    var fetch : Job? = null
 
     fun fetch(restaurantId: Int) {
-        viewModelScope.launch {
-            fetchReviewUseCase.invoke(restaurantId).collect {
+        fetch?.cancel()
+        fetch = viewModelScope.launch {
+            fetchReviewUseCase.invoke(restaurantId).collectLatest {
+                Log.d(tag, "collected : ${it.map { 
+                    "reviewId: ${it.reviewId}, isLike: ${it.isLike}, createDate: ${it.createDate}" 
+                }}")
                 uiState = it
             }
         }
